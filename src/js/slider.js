25e8e9dc101bec8aa66d570d348c83049aac4931
Liza <yemelyanova@list.ru>
2023-05-30 14:27:03 +0300

    ф

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,53 +1,17 @@
-// new Swiper('.image-slider',{
-//     touchRadio: 1,
-
-//     touchAngle: 45,
-
-//     grabCursor: true,
-
-//     slideToClickedSlide: true,
-
-//     mousewheel: {
-//         sensitivity: 1,
-//         eventsTarget: ".image-slider"
-//     },
-
-//     slidesPerView: 5,
-
-//     spaceBetween: 10,
-
-//     centeredSlides: true,
-
-//     slidesPerGroup: 1,
-
-//     initialSlide: 2,
-
-//     loop: true,
-
-//     loopedSlides: 2,
-
-//     speed: 800,
-
-// //     effect: 'coverflow',
-
-// //     coverflowEffect: {
-// //     rotate: 0,
-// //     stretch: 50,
-// //     slideShadows: false,
-// //     },
-// });
-
 var swiper = new Swiper(".mySwiper", {
-    effect: "coverflow",
-    grabCursor: true,
-    centeredSlides: true,
-    slidesPerView: "auto",
-    coverflowEffect: {
-      rotate: 15,
-      stretch: 0,
-      depth: 300,
-      modifier: 1,
-      slideShadows: true,
-    },
-    loop: true,
-  });
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: '3',
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 300,
+    modifier: 1,
+    slideShadows: false,
+  },
+  loop: true,
+  slideToClickedSlide: true,
+  loopedSlides: 4,
+  initialSlide: 2,
+});
